5e1b31ed73ab1bb8565d27cb810e87ff2f8ca3bf
GhostHunterrr <106083433+GhostHunterrr@users.noreply.github.com>
2023-04-27 17:27:44 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,10 +5,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/banana_RMX2061.mk
+    $(LOCAL_DIR)/lineage_RMX2061.mk
 
 
 COMMON_LUNCH_CHOICES := \
-   banana_RMX2061-user \
-    banana_RMX2061-userdebug \
-    cherish_RMX2061-eng
+   lineage_RMX2061-user \
+    lineage_RMX2061-userdebug \
+    lineage_RMX2061-eng
